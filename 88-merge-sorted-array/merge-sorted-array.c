56e42fcde3ac04b3ba7dc4a4a8df46571d5f2e83
Arif Zulfugarov <74620494+arifzulfugarov@users.noreply.github.com>
2026-02-15 19:29:11 +0100

    Time: 0 ms (100.00%) | Memory: 10.3 MB (12.00%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.c
+++ 88-merge-sorted-array/merge-sorted-array.c
@@ -1,29 +1,26 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     
 
-    int i = m - 1;
-    int j = n - 1;
-    int k = m+n-1;
+int right = nums1Size - 1;
 
-    while (i >= 0 && j >=0) {
-        if (nums1[i] > nums2[j]){
-        nums1[k] = nums1[i];
-        i--;
+while (m > 0 && n > 0){
+
+    if (nums1[m-1] > nums2[n-1]){
+        nums1[right] = nums1[m-1];
+        m--;
     }else{
-        nums1[k] = nums2[j];
-        j--;
+        nums1[right] = nums2[n-1];
+        n--;
     }
-    k--;
 
-    }
+    right--;
     
-
-while (j >= 0) {
-    nums1[k] = nums2[j];
-    j--;
-    k--;
 }
 
-
+while (n > 0) {
+    nums1[right] = nums2[n-1];
+    n--;
+    right--;
+}
 
 }

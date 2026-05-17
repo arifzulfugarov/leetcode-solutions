4ab56781cadc449347c8ceb5dd0923048c0f23bf
Arif Zulfugarov <74620494+arifzulfugarov@users.noreply.github.com>
2026-05-17 18:03:44 +0200

    Time: 3 ms (62.15%) | Memory: 67.2 MB (21.52%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.kt
+++ 53-maximum-subarray/maximum-subarray.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun maxSubArray(nums: IntArray): Int {
         
-        var maxSoFar = nums[0]
-        var currentSum = nums[0]
+       var maxSoFar = nums[0]
+       var currentSum = nums[0]
 
-        for (i in 1 until nums.size) {
+       for (i in 1 until nums.size) {
 
             currentSum = maxOf(nums[i], currentSum + nums[i])
 
             maxSoFar = maxOf(maxSoFar, currentSum)
-        }
+       }
 
-        return maxSoFar
-
+       return maxSoFar
     }
 }

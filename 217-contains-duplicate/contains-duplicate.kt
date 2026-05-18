4d7d9be972c2a087023b1ab0531934237dbb2fad
Arif Zulfugarov <74620494+arifzulfugarov@users.noreply.github.com>
2026-05-18 15:58:19 +0200

    Time: 22 ms (63.42%) | Memory: 61.2 MB (90.73%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.kt
+++ 217-contains-duplicate/contains-duplicate.kt
@@ -2,21 +2,17 @@ class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
         
 
-       val countMap = mutableMapOf<Int, Int>()
+        val seen = mutableSetOf<Int>()
 
-    for (num in nums) {
-        // Get the current count from the map (default to 0 if not present) and add 1
-        val currentCount = countMap.getOrDefault(num, 0) + 1
-        
-        // Your logic: If count is greater than 1, return true
-        if (currentCount > 1) {
-            return true
+        for (num in nums) {
+
+            if (seen.contains(num)){
+                return true
+            }
+
+            seen.add(num)
         }
         
-        // Save the updated count back into the map
-        countMap[num] = currentCount
-    }
-
-    return false
+        return false
     }
 }

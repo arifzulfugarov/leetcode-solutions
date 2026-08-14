97593fd34b0920cc748c85ac79728ac5f379729b
Arif Zulfugarov <74620494+arifzulfugarov@users.noreply.github.com>
2026-08-14 19:42:28 +0200

    Time: 3 ms (60.00%) | Memory: 41.5 MB (100.00%) - LeetSync

---
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun maximumLengthSubstring(s: String): Int {
+        
+
+        //sliding window?
+        //e.g. take b then c then b then b skip one if count[char] > 2 then skip char and start again
+
+        var maxLength = 0
+        var left = 0
+        val counts = IntArray(26)
+
+        for (right in s.indices) {
+            val charRight = s[right] - 'a'
+            counts[charRight]++
+
+            while (counts[charRight] > 2) {
+                val charLeft = s[left] - 'a'
+                counts[charLeft]--
+                left++
+            }
+
+
+            maxLength = max(maxLength, right - left + 1)
+        }
+
+        return maxLength
+    }
+}
